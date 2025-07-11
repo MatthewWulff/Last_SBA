d4eb25684f46af16bf02b99d48b71243e565eae8
caspr117 <caspr117@gmail.com>
2025-07-11 16:58:47 -0400

    fetching poke list data

--- poke-app/src/Home.jsx
+++ poke-app/src/Home.jsx
@@ -1,2 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+export default function Home(){
+    const[list, setList] = useState([])
+
+    useEffect(() => {
+        async function pokeList(){
+            const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+            const list = await res.json()
+            setList(list.results)
+        }
+       pokeList()
+    },[])
+}
+
